4fc8b48c011d793219b2121e1d6538e6f03fddc0
Saurabh Dev <ssdev338@gmail.com>
2021-09-24 22:46:44 +0530

    UpdateSample

---
+++ SampleJava/src/samplepackage/Sampleclass.java
@@ -0,0 +1,12 @@
+package samplepackage;
+
+public class Sampleclass {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello Saurabh");
+
+	}
+
+}
